16ce14ee89410eb40446dd513ede2dd5898561e5
Diana <diana.dev777@gmail.com>
2025-04-22 12:34:14 +0500

    create routes for items, places

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 const categories = require('./app/categories.js');
+const items = require('./app/items.js');
+const places = require('./app/places.js');
 const fileCategoriesDb = require('./fileCategoriesDb.js');
+const fileItemsDb = require('./fileItemsDb.js');
+const filePlacesDb = require('./filePlacesDb.js');
+
 const port = 8000;
 
 async function start() {
   app.use(express.json());
   app.use(express.static('public'));
   await fileCategoriesDb.init();
+  await fileItemsDb.init();
+  await filePlacesDb.init();
   app.use('/categories', categories);
+  app.use('/items', items);
+  app.use('/places', places);
 
   app.listen(port, () => {
     console.log(`Server is running on ${port}`);
